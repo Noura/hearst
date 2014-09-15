ce46d3ac679c2fe1c2e7374f7ea10d9345ab188d
Noura <culurienneldoreth@gmail.com>
2014-09-15 16:09:49 -0700

    changed tabs to spaces. sorry to be picky.

--- infinite.py
+++ infinite.py
@@ -10,27 +10,27 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
 
-	user_query = request.form['q']
+    user_query = request.form['q']
 
-	url = 'https://apis-qa.berkeley.edu/hearst_museum/select'
-	headers = {}
+    url = 'https://apis-qa.berkeley.edu/hearst_museum/select'
+    headers = {}
 
-	# todo: store these as environment vars
-	headers['app_key'] = "bd4e742fb930b51e2e6637f415f1742f"
-	headers['app_id'] = "26458f3f"
+    # todo: store these as environment vars
+    headers['app_key'] = "bd4e742fb930b51e2e6637f415f1742f"
+    headers['app_id'] = "26458f3f"
 
-	params = {
-	    #'q': 'objculturetee_txt:Arctic',
-	    #'q': 'objmaterials_txt:gold',
-	    'q': 'objname_txt:' + user_query,
-	    'wt': 'json',
-	    'indent': True,
-	    #'facet': 'true',
-	    #'facet.field':'objculturetree_ss'
-	    }
-	r = requests.get(url, params=params, headers=headers)
+    params = {
+        #'q': 'objculturetee_txt:Arctic',
+        #'q': 'objmaterials_txt:gold',
+        'q': 'objname_txt:' + user_query,
+        'wt': 'json',
+        'indent': True,
+        #'facet': 'true',
+        #'facet.field':'objculturetree_ss'
+        }
+    r = requests.get(url, params=params, headers=headers)
 
-	return json.dumps(json.loads(r.text), indent=4)
+    return json.dumps(json.loads(r.text), indent=4)
 
 if __name__ == '__main__':
     app.run(debug=True)
