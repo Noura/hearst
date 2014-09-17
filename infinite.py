420489e37b8dd9ca5cbea6f8f7f026e86e80e4b9
Noura <culurienneldoreth@gmail.com>
2014-09-17 15:47:31 -0700

    minor refactoring: api_search call

--- infinite.py
+++ infinite.py
@@ -3,45 +3,44 @@ import requests, json
 
 app = Flask(__name__)
 
+url = 'https://apis-qa.berkeley.edu/hearst_museum/select'
+headers = {
+        'app_key': "bd4e742fb930b51e2e6637f415f1742f",
+        'app_id': "26458f3f"
+}
+def img_url(blob_ss):
+    return 'https://dev.cspace.berkeley.edu/pahma_project/imageserver/blobs/' + blob_ss + '/derivatives/OriginalJpeg/content'
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# wrapper for Hearst API query
+def api_search(params):
+    params['wt'] = 'json'
+    r = requests.get(url, params=params, headers=headers)
+    return json.loads(r.text)
+
 @app.route('/search', methods=['POST'])
-def search():
-
+def artifacts_search():
+    n_results = 10
     user_query = request.form['q']
-    print 'searching for user_query', user_query
-
-    # we will return our own nicely formatted response
-    # of nice artifacts that have images and good descriptions etc
-    artifacts = []
-    want_n = 10
-
-    url = 'https://apis-qa.berkeley.edu/hearst_museum/select'
-    headers = {}
-
-    # todo: store these as environment vars
-    headers['app_key'] = "bd4e742fb930b51e2e6637f415f1742f"
-    headers['app_id'] = "26458f3f"
 
     params = {
-        'q': 'objname_txt:' + user_query,
-        'wt': 'json',
-        'start': 0,
+            'q': 'objname_txt:' + user_query,
+            'start': 0
     }
 
-    while len(artifacts) < want_n:
-        print 'about to search with params', params
-        r = requests.get(url, params=params, headers=headers)
-        d = json.loads(r.text)
-
+    artifacts = []
+    while len(artifacts) < n_results:
+        d = api_search(params)
         if len(d['response']['docs']) == 0:
-            break;
+            break
 
         params['start'] += len(d['response']['docs'])
 
         for artifact in d['response']['docs']:
+            print 'verbose thing', json.dumps(artifact, indent=4)
             if 'objdescr_s' not in artifact \
                     or 'objname_txt' not in artifact \
                     or 'blob_ss' not in artifact:
@@ -57,13 +56,8 @@ def search():
                 'img_url': imgurl,
             })
 
-    print json.dumps(artifacts, indent=4)
-
     data = json.dumps({'query': user_query,'artifacts': artifacts}, indent=4)
     return data
-
-def img_url(blob_ss):
-    return 'https://dev.cspace.berkeley.edu/pahma_project/imageserver/blobs/' + blob_ss + '/derivatives/OriginalJpeg/content'
 
 if __name__ == '__main__':
     app.run(debug=True)
